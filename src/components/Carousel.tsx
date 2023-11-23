1b8402add1fde55f1d7cc846643beecb7023bdc3
irumaru <jibakomaorochi13@outlook.jp>
2023-11-23 18:36:35 +0000

    コンテンツの追加

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,23 +6,18 @@ export const HomeCarousel = (props) => {
           <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true}>
                 <div className="carousel-item hidden-preview aspect-[5/3] overflow-hidden">
                     {props.im_1}
-                    <p className="legend">Legend 1</p>
                 </div>
                 <div className="carousel-item hidden-preview aspect-[5/3] overflow-hidden">
                     {props.im_2}
-                    <p className="legend">Legend 2</p>
                 </div>
                 <div className="carousel-item hidden-preview aspect-[5/3] overflow-hidden">
                     {props.im_3}
-                    <p className="legend">Legend 2</p>
                 </div>
                 <div className="carousel-item hidden-preview aspect-[5/3] overflow-hidden">
                     {props.im_4}
-                    <p className="legend">Legend 2</p>
                 </div>
                 <div className="carousel-item hidden-preview aspect-[5/3] overflow-hidden">
                     {props.im_5}
-                    <p className="legend">Legend 2</p>
                 </div>
           </Carousel>
   )
