980c06f0d3439d5b2f2788ba4d2fdb4977dcb8b2
irumaru <jibakomaorochi13@outlook.jp>
2023-11-23 18:40:38 +0000

    pagesへデプロイ

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+// buildホストから環境変数を取得
+const BASE = process.env.BASE;
+const SITE = process.env.SITE ? process.env.SITE : "http://127.0.0.1:4321/";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: SITE,
+  base: BASE,
 });
